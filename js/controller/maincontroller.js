05fcac5e929d536631a9554f889cd0fcfd7a32b3
Shadowhunder <sharma.sawan40@gmail.com>
2017-05-21 13:16:33 +0530

    Create maincontroller.js

---
+++ js/controller/maincontroller.js
@@ -0,0 +1,25 @@
+app.controller('mainCtrl', [ "$scope", function($scope) {
+
+	$scope.userData = [ {
+		"name" : "Chris Evans",
+		"designation" : "Software Engineer",
+		"experience" : 2.5
+	}, {
+		"name" : "Jack Ryan",
+		"designation" : "Assistant Software Engineer",
+		"experience" : 1.5
+	}, {
+		"name" : "Marga",
+		"designation" : "Senior Software Engineer",
+		"experience" : 5
+	}, {
+		"name" : "Emily",
+		"designation" : "Consultant",
+		"experience" :8
+	} ]
+
+
+
+
+
+} ]);
